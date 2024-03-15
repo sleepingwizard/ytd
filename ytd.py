d55997cdeb2510cae7d75f5e6cd780f1e27c1a3f
Aryan-81 <131597580+Aryan-81@users.noreply.github.com>
2024-03-15 13:32:39 +0530

    connverted to objecte oriented formate

--- ytd.py
+++ ytd.py
@@ -3,53 +3,82 @@ from pytube import YouTube
 from tkinter import filedialog
 from tkinter import ttk
 
-# Save Directory Button
-def select_directory():
-    directory = filedialog.askdirectory()
-    save_dir_label.config(text=directory)
-    return directory
 
-def download_video():
-    url = url_entry.get()
-    directory = save_dir_label.cget("text")  # Get the selected directory from the label
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("YouTube Video Converter")
+        self.initialize_ui()
+
     
-    if url and directory:
-        yt = YouTube(url, on_progress_callback=progress_callback)
-        video = yt.streams.get_highest_resolution()
-        video.download(directory)
-        status_label.config(text="Download successful!")
-    else:
-        status_label.config(text="Please provide both URL and save directory.")
- 
-def progress_callback(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
-    progress = (bytes_downloaded / total_size) * 100
-    pb['value'] = progress
 
-window = tk.Tk()
-window.title("YouTube Video Downloader")
-window.geometry("500x500")
+    def initialize_ui(self):
+        self.configure_window()  
+        self.create_widgets()
 
-url_label = tk.Label(window, text="Enter Youtube URL: ")
-url_label.pack()
+    def configure_window(self):
+        self.iconbitmap("assset/logo.ico")
+        # Set the window size
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        window_width = int(screen_width * 0.6)  # 60% of screen width
+        window_height = int(screen_height * 0.6)  # 60% of screen height
+        window_size = f"{window_width}x{window_height}"
+        self.geometry(window_size)
+        self.resizable(False,False)
+        # background image
+        image_path = "assset/background.png"
+        bg_image = tk.PhotoImage(file=image_path)
+        background_label = tk.Label(self, image=bg_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-url_entry = tk.Entry(window, width=50)
-url_entry.pack()
+    def create_widgets(self):
+        self.url_label = tk.Label(self, text="Enter Youtube URL: ")
+        self.url_label.pack()
 
-select_button = tk.Button(window, text="Select Save Directory", command=select_directory)
-select_button.pack()
+        self.url_entry = tk.Entry(self, width=50)
+        self.url_entry.pack()
 
-save_dir_label = tk.Label(window, text="")
-save_dir_label.pack()
+        self.select_button = tk.Button(self, text="Select Save Directory", command=self.select_directory)
+        self.select_button.pack()
 
-download_button = tk.Button(window, text="Download", command=download_video)
-download_button.pack()
+        self.save_dir_label = tk.Label(self, text="")
+        self.save_dir_label.pack()
 
-status_label = tk.Label(window, text="")
-status_label.pack()
+        self.download_button = tk.Button(self, text="Download", command=self.download_video)
+        self.download_button.pack()
 
-pb = ttk.Progressbar(window, orient='horizontal', length=200, mode='determinate')
-pb.pack()
+        self.status_label = tk.Label(self, text="")
+        self.status_label.pack()
 
-window.mainloop()
+        self.pb = ttk.Progressbar(self, orient='horizontal', length=200, mode='determinate')
+        self.pb.pack()
+
+    def select_directory(self):
+        directory = filedialog.askdirectory()
+        self.save_dir_label.config(text=directory)
+        return directory
+
+    def download_video(self):
+        url = self.url_entry.get()
+        directory = self.save_dir_label.cget("text")
+
+        if url and directory:
+            yt = YouTube(url, on_progress_callback=self.progress_callback)
+            video = yt.streams.get_highest_resolution()
+            video.download(directory)
+            self.status_label.config(text="Download successful!")
+        else:
+            self.status_label.config(text="Please provide both URL and save directory.")
+
+    def progress_callback(self, stream, chunk, bytes_remaining):
+        total_size = stream.filesize
+        bytes_downloaded = total_size - bytes_remaining
+        progress = (bytes_downloaded / total_size) * 100
+        self.pb['value'] = progress
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
